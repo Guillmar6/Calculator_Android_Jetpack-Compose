e88b12bd1124f61633c2abc1eca7c422f11faf11
Guillmar6 <css.guillmarreyes@gmail.com>
2026-07-01 23:25:11 +0800

    UI updated and added button functionality

--- app/src/main/java/com/guillmar/calculator/MainActivity.kt
+++ app/src/main/java/com/guillmar/calculator/MainActivity.kt
@@ -1,24 +1,39 @@
 package com.guillmar.calculator
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.guillmar.calculator.ui.theme.CalculatorTheme
 
@@ -26,7 +41,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var displayNums by remember { mutableStateOf("0") }
+            val myContext = LocalContext.current
+            val displayScrollState = rememberScrollState()
+            var displayNums: MutableState<String> = remember { mutableStateOf("0") }
+
+            LaunchedEffect(displayNums.value) {
+                displayScrollState.animateScrollTo(displayScrollState.maxValue)
+            }
+
             CalculatorTheme() {
                 Column(
                     modifier = Modifier
@@ -35,13 +57,24 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Bottom,
                     horizontalAlignment = Alignment.End
                 ) {
-                    Text(
-                        displayNums,
-                        color = MaterialTheme.colorScheme.secondary,
-                        fontSize = 100.sp,
+                    Column(
                         modifier = Modifier
-                    )
-                    BtnNumbers()
+                            .fillMaxSize()
+                    ) {
+                        Text(
+                            displayNums.value,
+                            color = MaterialTheme.colorScheme.secondary,
+                            fontSize = 100.sp,
+                            textAlign = TextAlign.End,
+                            maxLines = 1,
+                            modifier = Modifier
+                                .background(Color.Blue)
+                                .fillMaxWidth()
+                                .weight(1f)
+                                .horizontalScroll(displayScrollState)
+                        )
+                        BtnNumbers(myContext, displayNums)
+                    }
                 }
             }
         }
@@ -49,18 +82,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BtnNumbers() {
+fun BtnNumbers(myContext: Context, dis: MutableState<String>) {
     val modifierWeight = 1f
     val modifierAspectRatio = 1f
     val textSize = 12.sp
     Column(
         modifier = Modifier
             .aspectRatio(1f / 1.25f)
+            .background(Color.Cyan)
     ) {
+        Spacer(
+            modifier = Modifier
+                .height(10.dp)
+        )
         Row() {
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
                 onClick = {
-                    onClickBtnType('C')
+                    onClickBtnType(myContext, 'C', dis)
                 },
                 modifier = Modifier
                     .weight(modifierWeight)
@@ -71,9 +113,13 @@ fun BtnNumbers() {
                     fontSize = textSize
                 )
             }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
                 onClick = {
-                    onClickBtnType('n')
+                    onClickBtnType(myContext, 'n', dis)
                 },
                 modifier = Modifier
                     .weight(modifierWeight)
@@ -84,9 +130,13 @@ fun BtnNumbers() {
                     fontSize = textSize
                 )
             }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
                 onClick = {
-                    onClickBtnType('%')
+                    onClickBtnType(myContext, '%', dis)
                 },
                 modifier = Modifier
                     .weight(modifierWeight)
@@ -97,9 +147,13 @@ fun BtnNumbers() {
                     fontSize = textSize
                 )
             }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
                 onClick = {
-                    onClickBtnType('/')
+                    onClickBtnType(myContext, '/', dis)
                 },
                 modifier = Modifier
                     .weight(modifierWeight)
@@ -110,11 +164,22 @@ fun BtnNumbers() {
                     fontSize = textSize
                 )
             }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
         }
-
+        Spacer(
+            modifier = Modifier
+                .height(10.dp)
+        )
         Row() {
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(7) },
+                onClick = { onClickBtnNumbers(myContext, "7", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -122,8 +187,12 @@ fun BtnNumbers() {
                 "7",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(8) },
+                onClick = { onClickBtnNumbers(myContext, "8", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -131,8 +200,12 @@ fun BtnNumbers() {
                 "8",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(9) },
+                onClick = { onClickBtnNumbers(myContext, "9", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -140,9 +213,14 @@ fun BtnNumbers() {
                 Text(
                     "9",
                     fontSize = textSize
-                ) }
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnType('x') },
+                onClick = { onClickBtnType(myContext, 'x', dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -152,11 +230,22 @@ fun BtnNumbers() {
                     fontSize = textSize
                 )
             }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
         }
-
+        Spacer(
+            modifier = Modifier
+                .height(10.dp)
+        )
         Row() {
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(4) },
+                onClick = { onClickBtnNumbers(myContext, "4", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -164,8 +253,12 @@ fun BtnNumbers() {
                 "4",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(5) },
+                onClick = { onClickBtnNumbers(myContext, "5", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -173,8 +266,12 @@ fun BtnNumbers() {
                 "5",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(6) },
+                onClick = { onClickBtnNumbers(myContext, "6", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -182,8 +279,12 @@ fun BtnNumbers() {
                 "6",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnType('-') },
+                onClick = { onClickBtnType(myContext, '-', dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -193,11 +294,22 @@ fun BtnNumbers() {
                     fontSize = textSize
                 )
             }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
         }
-
+        Spacer(
+            modifier = Modifier
+                .height(10.dp)
+        )
         Row() {
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(1) },
+                onClick = { onClickBtnNumbers(myContext, "1", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -205,8 +317,12 @@ fun BtnNumbers() {
                 "1",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(2) },
+                onClick = { onClickBtnNumbers(myContext, "2", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -214,8 +330,12 @@ fun BtnNumbers() {
                 "2",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(3) },
+                onClick = { onClickBtnNumbers(myContext, "3", dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -223,8 +343,12 @@ fun BtnNumbers() {
                 "3",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnType('+') },
+                onClick = { onClickBtnType(myContext, '+', dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -234,11 +358,22 @@ fun BtnNumbers() {
                     fontSize = textSize
                 )
             }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
         }
-
+        Spacer(
+            modifier = Modifier
+                .height(10.dp)
+        )
         Row() {
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnNumbers(0) },
+                onClick = { onClickBtnNumbers(myContext, "0", dis) },
                 modifier = Modifier
                     .weight(2f)
                     .aspectRatio(1f / 0.5f)
@@ -246,8 +381,12 @@ fun BtnNumbers() {
                 "0",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnDec(".") },
+                onClick = { onClickBtnNumbers(myContext, ".", dis) },
                 modifier = Modifier
                     .weight(1f)
                     .aspectRatio(1f)
@@ -255,8 +394,12 @@ fun BtnNumbers() {
                 ".",
                 fontSize = textSize
             ) }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
             Button(
-                onClick = { onClickBtnType('=') },
+                onClick = { onClickBtnType(myContext, '=', dis) },
                 modifier = Modifier
                     .weight(modifierWeight)
                     .aspectRatio(modifierAspectRatio)
@@ -266,10 +409,34 @@ fun BtnNumbers() {
                     fontSize = textSize
                 )
             }
+            Spacer(
+                modifier = Modifier
+                    .width(10.dp)
+            )
         }
+        Spacer(
+            modifier = Modifier
+                .height(10.dp)
+        )
     }
 }
 
-fun onClickBtnNumbers(num: Int) {}
-fun onClickBtnDec(dec: String) {}
-fun onClickBtnType(type: Char) {}
+fun onClickBtnNumbers(myContext: Context, num: String, dis: MutableState<String>) {
+    if(dis.value == "0") {
+        if(num == ".") return
+        if(num != "0") dis.value = num
+        return
+    }
+    if(num == "." && dis.value.contains(".")) return
+    dis.value += num
+}
+fun onClickBtnType(myContext: Context, type: Char, dis: MutableState<String>) {
+    when {
+        type == 'C' -> {dis.value = "0"}
+        type == '=' -> {}
+        type == '+' -> {}
+        type == '-' -> {}
+        type == '/' -> {}
+        type == 'x' -> {}
+    }
+}
